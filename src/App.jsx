708f0c15b96e9da4ea1cbd52ccd04d9ffdf31cce
Mehmet Nur CEYLAN <ceylanmehmetnur92@gmail.com>
2025-08-20 20:56:53 +0300

    react-components

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/header";
+import MovieList from "./components/MovieList";
+
+export default function App(){
+  return(
+  <>
+    <Header/>
+    <MovieList/>
+  </>
+);
+}
---
+++ src/data.js
@@ -0,0 +1,30 @@
+ export const movie_list=[
+  {
+    image:"1.jpg",
+    title:"Kaptan amerika",
+    description:"Güzel bir film",
+    is_active:true,
+    is_new:true,
+  },
+  {
+    image:"2.jpg",
+    title:"Araba hırsızları",
+    description:"Güzel bir film",
+    is_active:true,
+    is_new:true,
+  },
+  {
+    image:"3.jpg",
+    title:"The codes of war",
+    description:"Güzel bir film",
+    is_active:true,
+    is_new:true,
+  },
+  {
+    image:"4.jpg",
+    title:"Moana 2",
+    description:"Güzel bir film",
+    is_active:true,
+    is_new:true,
+  },
+  ]
